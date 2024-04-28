953fe6b3cbc37c657618a2fcc84a024f03f6ab8c
MiguelbrmFreitas <miguelbrmfreitas@gmail.com>
2024-04-28 18:58:38 -0300

    - Added GetSportEventsUseCase

---
+++ domain/src/main/java/com/example/domain/usecase/GetSportEventsUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.domain.usecase
+
+import com.example.domain.repository.SportEventsRepository
+
+class GetSportEventsUseCase(private val repository: SportEventsRepository) {
+
+    suspend operator fun invoke() = repository.getSportEvents()
+
+}
