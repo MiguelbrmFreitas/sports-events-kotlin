b6def1c554f19474c5128aa27a9da373dcf37674
MiguelbrmFreitas <miguelbrmfreitas@gmail.com>
2024-04-28 17:45:30 -0300

    - Updated dependencies for data layer

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -37,6 +38,17 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.moshi)
+    implementation(libs.retrofit.converter.scalar)
+    implementation(libs.moshi)
+    implementation(libs.moshi.adapters)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.room)
+    annotationProcessor(libs.room.compiler)
+    ksp(libs.room.compiler)
+    implementation(libs.logging.interceptor)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
