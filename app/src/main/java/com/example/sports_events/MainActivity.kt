b50100939211ee6fc00740929b3467890e59c400
MiguelbrmFreitas <miguelbrmfreitas@gmail.com>
2024-04-30 04:51:49 -0300

    - Added additional logic at GetSportsUseCase to filter out past events and show only upcoming events

--- app/src/main/java/com/example/sports_events/MainActivity.kt
+++ app/src/main/java/com/example/sports_events/MainActivity.kt
@@ -6,25 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.example.sports_events.ui.components.SportsScreen
 import com.example.sports_events.ui.theme.SportseventsTheme
-import com.example.sports_events.viewmodel.SportsViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
-
-//    private val sportsViewModel: SportsViewModel by viewModel()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            val sports by sportsViewModel.sportsState.collectAsState()
-
             SportseventsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
--- domain/src/main/java/com/example/domain/usecase/GetSportsUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/GetSportsUseCase.kt
@@ -1,11 +1,30 @@
 package com.example.domain.usecase
 
+import com.example.domain.core.ResponseStatus
+import com.example.domain.model.Sport
 import com.example.domain.repository.SportsRepository
 
 class GetSportsUseCase(
     private val repository: SportsRepository
 ) {
+    suspend operator fun invoke(): ResponseStatus<List<Sport>> {
+        repository.getSports().also { responseStatus ->
+            return when(responseStatus) {
+                is ResponseStatus.Success -> {
+                    val currentTime = System.currentTimeMillis()/1000L
+                    val filteredSports = responseStatus.result.map { sport ->
+                        val filteredEvents = sport.events.filter { event ->
+                            (event.timestamp - currentTime) >= 0
+                        }
+                        sport.copy(events = filteredEvents)
+                    }
+                    ResponseStatus.Success(filteredSports)
+                }
 
-    suspend operator fun invoke() = repository.getSports()
-
+                else -> {
+                    responseStatus
+                }
+            }
+        }
+    }
 }
